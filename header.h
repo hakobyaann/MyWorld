8ffcf4c1ec950a08a5169d5024e74832a63a20ac
hakobyaann <109814279+hakobyaann@users.noreply.github.com>
2023-02-03 18:03:21 +0400

    Update header.h

--- header.h
+++ header.h
@@ -23,22 +23,3 @@ public:
     std::vector<int>stomach;
     int eats(Grass, const bool);
 };
-
-void movement()
-{
-    std::vector<int>place(6, 0);
-    for (int i = 0; i < place.size(); ++i)
-    {
-        if (i % 2)
-        {
-            sleep(1);
-            std::cout << "The Froggy moves to the left" << std::endl;
-        }
-        else
-        {
-            sleep(1);
-            std::cout << "The Froggy moves to the right" << std::endl;
-
-        }
-    }
-}
